8beab81c6a3e9e8dcd89aa5d8cd4d9993fbb173b
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-03 06:49:57 -0500

    Added movement controls to free look state

--- Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -13,7 +13,11 @@ public class PlayerFreeLookState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        
+        Vector3 movement = new Vector3();
+        movement.x = stateMachine.InputReader.MovementValue.x;
+        movement.y = 0;
+        movement.z = stateMachine.InputReader.MovementValue.y;
+        stateMachine.transform.Translate(movement * deltaTime);
     }
 
     public override void Exit()
