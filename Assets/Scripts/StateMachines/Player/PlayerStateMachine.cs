23b54e8de8d6c4175b9b19bbab42baa8133b8917
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-03 07:11:03 -0500

    Set initial player state to free look

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -7,4 +7,9 @@ public class PlayerStateMachine : StateMachine
     [field:SerializeField] public InputReader InputReader { get; private set; }
     [field:SerializeField] public CharacterController Controller { get; private set; }
     [field:SerializeField] public float FreeLookMovementSpeed { get; private set; }
+
+    void Start()
+    {
+        SwitchState(new PlayerFreeLookState(this));
+    }
 }
