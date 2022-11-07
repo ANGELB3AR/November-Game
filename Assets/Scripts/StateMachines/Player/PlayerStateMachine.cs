105f23b8a28a2f438f44d2eb465956c55d25fb4a
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-07 11:18:27 -0600

    Added comments to categorize player state machine references

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    // Component References
     [field:SerializeField] public InputReader InputReader { get; private set; }
     [field:SerializeField] public CharacterController Controller { get; private set; }
     [field:SerializeField] public Animator Animator { get; private set; }
+
+    // External References
     [field:SerializeField] public Transform MainCameraTransform { get; private set; }
+
+    // Variable References
     [field:SerializeField] public float FreeLookMovementSpeed { get; private set; }
     [field:SerializeField] public float TargetingMovementSpeed { get; private set; }
 
