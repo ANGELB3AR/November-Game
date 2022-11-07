0b5fe6e4d1f432d27fd84cbf44ebee2fcfff7975
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-07 11:44:08 -0600

    Added some targeting logic

--- Assets/Scripts/Combat/Targeter.cs
+++ Assets/Scripts/Combat/Targeter.cs
@@ -1,8 +1,38 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Targeter : MonoBehaviour
 {
+    [SerializeField] CinemachineTargetGroup targetGroup;
 
+    List<Target> targets = new List<Target>();
+    Camera mainCamera;
+
+    public Target CurrentTarget { get; private set; }
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+        targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }
+        RemoveTarget(target);
+    }
+
+    void RemoveTarget(Target target)
+    {
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
+    }
 }
