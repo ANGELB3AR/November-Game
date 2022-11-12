7bbdba6448b8c6da59dbe854248df81d59e4caa9
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-12 12:34:54 -0600

    Created attack combo script

---
+++ Assets/Scripts/Combat/AttackCombos.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCombos : MonoBehaviour
+{
+    [field:SerializeField] public Attack[] Combo { get; private set; }
+}
