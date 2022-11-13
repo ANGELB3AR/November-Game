eb0e643cdb2665d51d51580a14456b1472139b50
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-13 12:48:37 -0600

    Retested damage counter - still not picking up on Attack class

--- Assets/Scripts/Combat/AttackCombos.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AttackCombos : MonoBehaviour
-{
-    [field:SerializeField] public Attack[] Combo { get; private set; }
-}
