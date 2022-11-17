8efbfecf88ae3eedad046df45da35d9343aa680c
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-17 13:02:55 -0600

    Removed IDamageModifier from Attack class - After further testing decided this is not going to work this way

--- Assets/Scripts/Combat/Attack.cs
+++ Assets/Scripts/Combat/Attack.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Attack : IDamageModifier
+public class Attack
 {
     [field:SerializeField] public string AnimationName { get; private set; }
-    [field:SerializeField] public float baseDamage { get; private set; }
-    [field:SerializeField] public float percentageBonusDamage { get; private set; }
     [field:SerializeField] public float TransitionDuration { get; private set; }
     [field:SerializeField] public float ComboAttackTime { get; private set; }
     [field:SerializeField] public int ComboIndex { get; private set; } = -1;
-
-    public IEnumerable<float> GetAdditiveModifiers()
-    {
-        yield return baseDamage;
-    }
-
-    public IEnumerable<float> GetPercentageModifiers()
-    {
-        yield return percentageBonusDamage;
-    }
 }
