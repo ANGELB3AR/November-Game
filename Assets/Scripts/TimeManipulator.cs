06c2a0010823ad0675097ea7aeac9eb908fc168c
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-25 12:22:15 -0600

    Created Time Manipulator script with public SlowTime() method

---
+++ Assets/Scripts/TimeManipulator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManipulator : MonoBehaviour
+{
+    [SerializeField] float normalTime;
+    [SerializeField] float slowedTime;
+    [SerializeField] float duration;
+
+    public void SlowTime()
+    {
+        StartCoroutine(SlowTimeEffect());
+    }
+
+    IEnumerator SlowTimeEffect()
+    {
+        Time.timeScale = slowedTime;
+        yield return new WaitForSeconds(duration);
+        Time.timeScale = normalTime;
+    }
+}
