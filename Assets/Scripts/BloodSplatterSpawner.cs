0a50aeab67eebafa1f98a4da0bcdecfa820c41de
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-11-29 09:27:09 -0600

    Tried adding a rigidbody to the ground to get blood splatter effect - No luck

--- Assets/Scripts/BloodSplatterSpawner.cs
+++ Assets/Scripts/BloodSplatterSpawner.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class BloodSplatterSpawner : MonoBehaviour
 {
     [SerializeField] GameObject bloodDecal;
+    [SerializeField] ParticleSystem bloodParticles;
+    //[SerializeField] Transform splatHolder;
+
+    List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(bloodDecal, other.transform.position, Quaternion.identity);
+        Debug.Log("Particle collision occurred");
+        ParticlePhysicsExtensions.GetCollisionEvents(bloodParticles, other, collisionEvents);
+
+        int count = collisionEvents.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject splat = Instantiate(bloodDecal, collisionEvents[i].intersection, Quaternion.identity) as GameObject;
+        }
     }
 }
