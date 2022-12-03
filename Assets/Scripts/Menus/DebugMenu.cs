093e6efe9bdb617df9b37520936b1c0180e24d52
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-12-03 11:05:00 -0600

    Playtested

--- Assets/Scripts/Menus/DebugMenu.cs
+++ Assets/Scripts/Menus/DebugMenu.cs
@@ -15,6 +15,7 @@ public class DebugMenu : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
+        GameManager.Instance.UpdateGameState(GameManager.GameState.Playing);
     }
 
     public void EquipSword()
