90b5684fb42359b5fabc23f3de77fe23c73fb422
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-25 10:12:33 -0500

    State now inherits from Network Behaviour

--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -28,6 +28,8 @@ public abstract class PlayerBaseState : State
 
     protected void Move(Vector3 direction, float speed, float deltaTime)
     {
+        if (!IsOwner) { return; }
+
         if (!stateMachine.Controller.isGrounded)
         {
             direction = ApplyGravity(direction, deltaTime);
--- Assets/Scripts/StateMachines/State.cs
+++ Assets/Scripts/StateMachines/State.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public abstract class State
+public abstract class State : NetworkBehaviour
 {
     public abstract void Enter();
     public abstract void Tick(float deltaTime);
